cc4ae0adc1988c132cba588160d5afe6632f7858
rafaelfang <cf797@mail.missouri.edu>
2014-11-26 15:53:56 -0600

    sign function

--- heavisideStepFunction.m
+++ heavisideStepFunction.m
@@ -2,13 +2,9 @@ function [ output ] = heavisideStepFunction( input )
 % this is the Heaviside Step Function
 % written by Chao Fang
 
-output=zeros(size(input));
-
-for i=1:size(input,2)
-    if input(i)>0
-        output(i) = 1;
-    else 
-        output(i) =0;
+    if input>0
+        output=1;
+    else
+        output=0;
     end
-end
 end
