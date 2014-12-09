91067fa776ebc8915731a4a4608fa58b93217793
rafaelfang <cf797@mail.missouri.edu>
2014-12-09 15:41:13 -0600

    smo runner

--- runnerSMO.m
+++ runnerSMO.m
@@ -6,29 +6,29 @@ clc
 rng(1);
 
 %% prepare training and testing dataset
-% load diabetes
-% 
-% trainIndex=1:round(0.8*size(diabetes,1));
-% trainAttribute=diabetes(trainIndex,2:end);
-% trainLabel=diabetes(trainIndex,1);
-% 
-% testIndex=round(0.8*size(diabetes,1))+1:size(diabetes,1);
-% testAttribute=diabetes(testIndex,2:end);
-% testLabel=diabetes(testIndex,1);
-% N=size(trainAttribute,1);
+load diabetes
+divideRate=0.8;
+trainIndex=1:floor(divideRate*size(diabetes,1));
+trainAttribute=diabetes(trainIndex,2:end);
+trainLabel=diabetes(trainIndex,1);
+
+testIndex=floor(divideRate*size(diabetes,1))+1:size(diabetes,1);
+testAttribute=diabetes(testIndex,2:end);
+testLabel=diabetes(testIndex,1);
+N=size(trainAttribute,1);
 
 
-trainAttribute=[ 0.5 0.5;
-                0.25 0.25;
-                1.5 1.5 ;
-                1.25 1.25 ];
-                
-trainAttribute=normc(trainAttribute);
-trainLabel=[1;
-    1;
-    -1;
-    -1];
-N=4;
+% trainAttribute=[ 0.5 0.5;
+%                 0.25 0.25;
+%                 1.5 1.5 ;
+%                 1.25 1.25 ];
+%                 
+% trainAttribute=normc(trainAttribute);
+% trainLabel=[1;
+%     1;
+%     -1;
+%     -1];
+% N=4;
 %% SMO
 C=0.9;
 tol=100;
@@ -54,12 +54,11 @@ end
 trainAcc=sum(trainLabel(:)==predTrain(:))/N;
 
 %% testing Accuracy
-testIndex=round(0.8*size(diabetes,1))+1:size(diabetes,1);
-testAttribute=diabetes(testIndex,2:end);
-testLabel=diabetes(testIndex,1);    
+
+
 predTest=zeros(size(testAttribute,1),1);
 for i=1:size(testAttribute,1)
-    predTest(i,1)=sign(w*trainAttribute(i,:)'+bVal);
+    predTest(i,1)=sign(w*testAttribute(i,:)'+bVal);
 end  
 
 testAcc=sum(testLabel(:)==predTest(:))/size(testAttribute,1);
